6867f10bcf4dbb8cefbe3652789f27a9c55cbe70
KazIgu <kaz.igu.21@gmail.com>
2014-12-30 04:50:28 +0900

    諸々ロジック修正

--- zen2han.py
+++ zen2han.py
@@ -1,33 +1,72 @@
 import sublime, sublime_plugin
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '')
-import zenhan
+
+ZEN2HAN = (
+    ("ァー", "ｧ-"), ("ィー", "ｨ-"), ("ゥー", "ｩ-"), ("ェー", "ｪ-"), ("ォー", "ｫ-"),
+    ("ッー", "ｯ-"), ("ャー", "ｬ-"), ("ュー", "ｭ-"), ("ョー", "ｮ-"),
+    ("ガー", "ｶﾞ-"), ("ギー", "ｷﾞ-"), ("グー", "ｸﾞ-"), ("ゲー", "ｹﾞ-"), ("ゴー", "ｺﾞ-"),
+    ("ザー", "ｻﾞ-"), ("ジー", "ｼﾞ-"), ("ズー", "ｽﾞ-"), ("ゼー", "ｾﾞ-"), ("ゾー", "ｿﾞ-"),
+    ("ダー", "ﾀﾞ-"), ("ヂー", "ﾁﾞ-"), ("ヅー", "ﾂﾞ-"), ("デー", "ﾃﾞ-"), ("ドー", "ﾄﾞ-"),
+    ("バー", "ﾊﾞ-"), ("ビー", "ﾋﾞ-"), ("ブー", "ﾌﾞ-"), ("ベー", "ﾍﾞ-"), ("ボー", "ﾎﾞ-"),
+    ("パー", "ﾊﾟ-"), ("ピー", "ﾋﾟ-"), ("プー", "ﾌﾟ-"), ("ペー", "ﾍﾟ-"), ("ポー", "ﾎﾟ-"),
+    ("ヴー", "ｳﾞ-"),
+    ("アー", "ｱ-"), ("イー", "ｲ-"), ("ウー", "ｳ-"), ("エー", "ｴ-"), ("オー", "ｵ-"),
+    ("カー", "ｶ-"), ("キー", "ｷ-"), ("クー", "ｸ-"), ("ケー", "ｹ-"), ("コー", "ｺ-"),
+    ("サー", "ｻ-"), ("シー", "ｼ-"), ("スー", "ｽ-"), ("セー", "ｾ-"), ("ソー", "ｿ-"),
+    ("ター", "ﾀ-"), ("チー", "ﾁ-"), ("ツー", "ﾂ-"), ("テー", "ﾃ-"), ("トー", "ﾄ-"),
+    ("ナー", "ﾅ-"), ("ニー", "ﾆ-"), ("ヌー", "ﾇ-"), ("ネー", "ﾈ-"), ("ノー", "ﾉ-"),
+    ("ハー", "ﾊ-"), ("ヒー", "ﾋ-"), ("フー", "ﾌ-"), ("ヘー", "ﾍ-"), ("ホー", "ﾎ-"),
+    ("マー", "ﾏ-"), ("ミー", "ﾐ-"), ("ムー", "ﾑ-"), ("メー", "ﾒ-"), ("モー", "ﾓ-"),
+    ("ヤー", "ﾔ-"), ("ユー", "ﾕ-"), ("ヨー", "ﾖ-"),
+    ("ラー", "ﾗ-"), ("リー", "ﾘ-"), ("ルー", "ﾙ-"), ("レー", "ﾚ-"), ("ロー", "ﾛ-"),
+    ("ワー", "ﾜ-"), ("ヲー", "ｦ-"), ("ンー", "ﾝ-"),
+
+    ("ァ", "ｧ"), ("ィ", "ｨ"), ("ゥ", "ｩ"), ("ェ", "ｪ"), ("ォ", "ｫ"),
+    ("ッ", "ｯ"), ("ャ", "ｬ"), ("ュ", "ｭ"), ("ョ", "ｮ"),
+    ("ガ", "ｶﾞ"), ("ギ", "ｷﾞ"), ("グ", "ｸﾞ"), ("ゲ", "ｹﾞ"), ("ゴ", "ｺﾞ"),
+    ("ザ", "ｻﾞ"), ("ジ", "ｼﾞ"), ("ズ", "ｽﾞ"), ("ゼ", "ｾﾞ"), ("ゾ", "ｿﾞ"),
+    ("ダ", "ﾀﾞ"), ("ヂ", "ﾁﾞ"), ("ヅ", "ﾂﾞ"), ("デ", "ﾃﾞ"), ("ド", "ﾄﾞ"),
+    ("バ", "ﾊﾞ"), ("ビ", "ﾋﾞ"), ("ブ", "ﾌﾞ"), ("ベ", "ﾍﾞ"), ("ボ", "ﾎﾞ"),
+    ("パ", "ﾊﾟ"), ("ピ", "ﾋﾟ"), ("プ", "ﾌﾟ"), ("ペ", "ﾍﾟ"), ("ポ", "ﾎﾟ"),
+    ("ヴ", "ｳﾞ"),
+    ("ア", "ｱ"), ("イ", "ｲ"), ("ウ", "ｳ"), ("エ", "ｴ"), ("オ", "ｵ"),
+    ("カ", "ｶ"), ("キ", "ｷ"), ("ク", "ｸ"), ("ケ", "ｹ"), ("コ", "ｺ"),
+    ("サ", "ｻ"), ("シ", "ｼ"), ("ス", "ｽ"), ("セ", "ｾ"), ("ソ", "ｿ"),
+    ("タ", "ﾀ"), ("チ", "ﾁ"), ("ツ", "ﾂ"), ("テ", "ﾃ"), ("ト", "ﾄ"),
+    ("ナ", "ﾅ"), ("ニ", "ﾆ"), ("ヌ", "ﾇ"), ("ネ", "ﾈ"), ("ノ", "ﾉ"),
+    ("ハ", "ﾊ"), ("ヒ", "ﾋ"), ("フ", "ﾌ"), ("ヘ", "ﾍ"), ("ホ", "ﾎ"),
+    ("マ", "ﾏ"), ("ミ", "ﾐ"), ("ム", "ﾑ"), ("メ", "ﾒ"), ("モ", "ﾓ"),
+    ("ヤ", "ﾔ"), ("ユ", "ﾕ"), ("ヨ", "ﾖ"),
+    ("ラ", "ﾗ"), ("リ", "ﾘ"), ("ル", "ﾙ"), ("レ", "ﾚ"), ("ロ", "ﾛ"),
+    ("ワ", "ﾜ"), ("ヲ", "ｦ"), ("ン", "ﾝ"),
+
+    ("。", "｡"), ("、", "､")
+)
+
+HAN2ZEN = lambda: ((h, z) for z, h in ZEN2HAN)
+
+def convert(text, *maps):
+    def replace(text, fr, to):
+        return text.replace(fr, to)
+
+    for m in maps:
+        if callable(m):
+            m = m()
+        elif isinstance(m, dict):
+            m = m.items()
+        for fr, to in m:
+            text = replace(text, fr, to)
+
+    return text
+
 class Zen2hanCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     for region in self.view.sel():
         select_texts = self.view.substr(region)
 
         if select_texts != "":
-            zen2han_text = zenhan.hz(select_texts)
-            han2zen_text = zenhan.zh(select_texts)
+            zen2han_text = convert(select_texts, ZEN2HAN)
             if select_texts != zen2han_text:
                 self.view.replace(edit, region, zen2han_text)
-            elif select_texts != han2zen_text:
-                self.view.replace(edit, region, han2zen_text)
-
-class Kana2kanaCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    for region in self.view.sel():
-        select_texts = self.view.substr(region)
-
-        if select_texts != "":
-            zen2han_text = zenhan.jz(select_texts)
-            han2zen_text = zenhan.zj(select_texts)
-            if select_texts != zen2han_text:
-                self.view.replace(edit, region, zen2han_text)
-            elif select_texts != han2zen_text:
-                self.view.replace(edit, region, han2zen_text)
 
 
 class Han2zenCommand(sublime_plugin.TextCommand):
@@ -36,7 +75,7 @@ class Han2zenCommand(sublime_plugin.TextCommand):
         select_texts = self.view.substr(region)
 
         if select_texts != "":
-            han2zen_text = zenhan.hz(select_texts)
+            han2zen_text = convert(select_texts, HAN2ZEN)
             if select_texts != han2zen_text:
                 self.view.replace(edit, region, han2zen_text)
 
